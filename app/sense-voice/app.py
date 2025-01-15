35ca34e26dd5b15328f94f8e43e8c8d4c4cc6f2c
eleven <yinji0208@gmail.com>
2025-01-15 15:43:19 +0800

    init

---
+++ app/sense-voice/app.py
@@ -0,0 +1,27 @@
+from funasr import AutoModel
+from funasr.utils.postprocess_utils import rich_transcription_postprocess
+
+model_dir = "/app/iic/SenseVoiceSmall"
+
+
+model = AutoModel(
+    model=model_dir,
+    trust_remote_code=False,
+    remote_code="./model.py",  
+    vad_model="fsmn-vad",
+    vad_kwargs={"max_single_segment_time": 30000},
+    device="cuda:0",
+)
+
+# en
+res = model.generate(
+    input=f"/app/example/test-cn.wav",
+    cache={},
+    language="zn",  # "zn", "en", "yue", "ja", "ko", "nospeech"
+    use_itn=True,
+    batch_size_s=60,
+    merge_vad=True,  #
+    merge_length_s=15,
+)
+text = rich_transcription_postprocess(res[0]["text"])
+print(text)
